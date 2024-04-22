b3d0847d6213921ab7e7b8bc292265211c15bb09
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2024-04-22 16:20:48 -0700

    Update schema.js

--- server/schema.js
+++ server/schema.js
@@ -1,67 +1,88 @@
-const Todo = require('./Todo');
+const Todo = require("./Todo");
 
-const { GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLID, GraphQLList, GraphQLSchema, GraphQLNonNull } = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLID,
+  GraphQLList,
+  GraphQLSchema,
+  GraphQLNonNull,
+} = require("graphql");
 
 const TodoType = new GraphQLObjectType({
-    name: 'Todo',
-    fields: () => ({
-        id: { type: GraphQLID },
-        title: { type: GraphQLString },
-        completed: { type: GraphQLBoolean }
-    })
+  name: "Todo",
+  fields: () => ({
+    id: { type: GraphQLID },
+    title: { type: GraphQLString },
+    completed: { type: GraphQLBoolean },
+  }),
 });
 
 const RootQueryType = new GraphQLObjectType({
-    name: 'Query',
-    fields: {
-        todos: {
-            type: new GraphQLList(TodoType),
-            resolve: (parent, args) => {
-                return Todo.find();
-            }
-        },
-        todo: {
-            type: TodoType,
-            args: { id: { type: GraphQLID } },
-            resolve: (parent, args) => {
-                return Todo.findById(args.id);
-            }
-        }
-    }
+  name: "Query",
+  fields: {
+    todos: {
+      type: new GraphQLList(TodoType),
+      resolve: (parent, args) => {
+        return Todo.find();
+      },
+    },
+    todo: {
+      type: TodoType,
+      args: { id: { type: GraphQLID } },
+      resolve: (parent, args) => {
+        return Todo.findById(args.id);
+      },
+    },
+  },
 });
 
 const mutation = new GraphQLObjectType({
-    name: 'Mutation',
-    fields: {
-        addTodo: {
-            type: TodoType,
-            args: {
-                title: { type: new GraphQLNonNull(GraphQLString) },
-                completed: { type: new GraphQLNonNull(GraphQLBoolean) }
-            },
-            resolve (parent, args)  {
-                const todo = new Todo({
-                    title: args.title,
-                    completed: false
-                });
+  name: "Mutation",
+  fields: {
+    addTodo: {
+      type: TodoType,
+      args: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        completed: { type: new GraphQLNonNull(GraphQLBoolean) },
+      },
+      resolve(parent, args) {
+        const todo = new Todo({
+          title: args.title,
+          completed: false,
+        });
 
-                return todo.save();
-            }
-        },
-        deleteTodo: {
-            type: TodoType,
-            args: {
-                id: { type: new GraphQLNonNull(GraphQLID) }
-            },
-            resolve (parent, args) {
-                return Todo.findByIdAndDelete(args.id);
-            }
-        },
-    }
+        return todo.save();
+      },
+    },
+    deleteTodo: {
+      type: TodoType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        return Todo.findByIdAndDelete(args.id);
+      },
+    },
+    updateTodo: {
+      type: TodoType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+        title: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        return Todo.findByIdAndUpdate(args.id, {
+          $set: {
+            title: args.title,
+          },
+        });
+      },
+    },
+  },
 });
-        
 
 module.exports = new GraphQLSchema({
-    query: RootQueryType,
-    mutation
+  query: RootQueryType,
+  mutation,
 });
